d64ae6dfa5cfef295ee19c24f5d3fc93d6cb6c71
jarynliu <liu.yanji@northeastern.edu>
2022-12-05 22:43:17 -0500

    homepage ui update

--- src/PrePage/HomePage.java
+++ src/PrePage/HomePage.java
@@ -10,6 +10,7 @@ import SignUpPage.UserSignPage;
 import SignUpPage.AnimalShelterSignPage;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import javax.swing.SwingConstants;
 
 /**
  *
@@ -38,38 +39,38 @@ public class HomePage extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jbtnmaster = new javax.swing.JButton();
-        jbtnhospital = new javax.swing.JButton();
-        jbtnbussiness = new javax.swing.JButton();
-        jbtnstray = new javax.swing.JButton();
+        jlbluser = new javax.swing.JLabel();
+        jlblhospital = new javax.swing.JLabel();
+        jlblbusiness = new javax.swing.JLabel();
+        jlblanimalshelter = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jbtnmaster.setText("User");
-        jbtnmaster.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jbtnmasterActionPerformed(evt);
+        jlbluser.setIcon(new javax.swing.ImageIcon("C:\\Users\\pkuho\\Desktop\\1.jpg")); // NOI18N
+        jlbluser.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jlbluserMouseClicked(evt);
             }
         });
 
-        jbtnhospital.setText("Hospital");
-        jbtnhospital.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jbtnhospitalActionPerformed(evt);
+        jlblhospital.setIcon(new javax.swing.ImageIcon("C:\\Users\\pkuho\\Desktop\\1.jpg")); // NOI18N
+        jlblhospital.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jlblhospitalMouseClicked(evt);
             }
         });
 
-        jbtnbussiness.setText("Business");
-        jbtnbussiness.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jbtnbussinessActionPerformed(evt);
+        jlblbusiness.setIcon(new javax.swing.ImageIcon("C:\\Users\\pkuho\\Desktop\\1.jpg")); // NOI18N
+        jlblbusiness.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jlblbusinessMouseClicked(evt);
             }
         });
 
-        jbtnstray.setText("Animal Shelter");
-        jbtnstray.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jbtnstrayActionPerformed(evt);
+        jlblanimalshelter.setIcon(new javax.swing.ImageIcon("C:\\Users\\pkuho\\Desktop\\1.jpg")); // NOI18N
+        jlblanimalshelter.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jlblanimalshelterMouseClicked(evt);
             }
         });
 
@@ -78,42 +79,39 @@ public class HomePage extends javax.swing.JFrame {
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(125, 125, 125)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jbtnbussiness, javax.swing.GroupLayout.DEFAULT_SIZE, 103, Short.MAX_VALUE)
-                    .addComponent(jbtnmaster, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(247, 247, 247)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jlbluser)
+                    .addComponent(jlblbusiness))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(222, 222, 222)
-                        .addComponent(jbtnhospital, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addGap(230, 230, 230)
-                        .addComponent(jbtnstray)))
-                .addContainerGap(230, Short.MAX_VALUE))
+                    .addComponent(jlblanimalshelter)
+                    .addComponent(jlblhospital))
+                .addContainerGap(267, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(187, 187, 187)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jbtnmaster)
-                    .addComponent(jbtnhospital))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 147, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jbtnbussiness)
-                    .addComponent(jbtnstray))
-                .addGap(214, 214, 214))
+                .addGap(129, 129, 129)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jlbluser, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jlblhospital, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jlblanimalshelter, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jlblbusiness, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(161, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
@@ -121,37 +119,37 @@ public class HomePage extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jbtnmasterActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnmasterActionPerformed
+    private void jlbluserMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jlbluserMouseClicked
         // TODO add your handling code here:
-        dispose();
+         dispose();
         UserSignPage mastersignpage = new UserSignPage();
         mastersignpage.setTitle("User Signup");
         mastersignpage.setVisible(true);
-    }//GEN-LAST:event_jbtnmasterActionPerformed
+    }//GEN-LAST:event_jlbluserMouseClicked
 
-    private void jbtnhospitalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnhospitalActionPerformed
+    private void jlblhospitalMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jlblhospitalMouseClicked
         // TODO add your handling code here:
         dispose();
         HospitalSignPage hospitalsignpage = new HospitalSignPage();
         hospitalsignpage.setTitle("Hospital Signup");
         hospitalsignpage.setVisible(true);
-    }//GEN-LAST:event_jbtnhospitalActionPerformed
+    }//GEN-LAST:event_jlblhospitalMouseClicked
 
-    private void jbtnbussinessActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnbussinessActionPerformed
+    private void jlblbusinessMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jlblbusinessMouseClicked
         // TODO add your handling code here:
-        dispose();
+         dispose();
         BusinessSignPage bussinesssignpage = new BusinessSignPage();
         bussinesssignpage.setTitle("Bussiness Signup");
         bussinesssignpage.setVisible(true);
-    }//GEN-LAST:event_jbtnbussinessActionPerformed
+    }//GEN-LAST:event_jlblbusinessMouseClicked
 
-    private void jbtnstrayActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnstrayActionPerformed
+    private void jlblanimalshelterMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jlblanimalshelterMouseClicked
         // TODO add your handling code here:
         dispose();
         AnimalShelterSignPage strayanimalssignpage = new AnimalShelterSignPage();
         strayanimalssignpage.setTitle("Shelter Signup");
         strayanimalssignpage.setVisible(true);
-    }//GEN-LAST:event_jbtnstrayActionPerformed
+    }//GEN-LAST:event_jlblanimalshelterMouseClicked
 
     /**
      * @param args the command line arguments
@@ -191,9 +189,9 @@ public class HomePage extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JButton jbtnbussiness;
-    private javax.swing.JButton jbtnhospital;
-    private javax.swing.JButton jbtnmaster;
-    private javax.swing.JButton jbtnstray;
+    private javax.swing.JLabel jlblanimalshelter;
+    private javax.swing.JLabel jlblbusiness;
+    private javax.swing.JLabel jlblhospital;
+    private javax.swing.JLabel jlbluser;
     // End of variables declaration//GEN-END:variables
 }
